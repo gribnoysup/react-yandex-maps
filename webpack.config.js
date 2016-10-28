c3351f05a89625e643a504f214dde049ec6e0e25
s.petushkov <s.petushkov@qiwi.ru>
2016-10-28 14:24:06 +0300

    Change spacing

--- webpack.config.js
+++ webpack.config.js
@@ -4,46 +4,46 @@ var path = require('path')
 var env = JSON.stringify(process.env)
 
 module.exports = {
-    entry: path.join(__dirname, 'src', 'index.js'),
+  entry: path.join(__dirname, 'src', 'index.js'),
 
-    module: {
-        loaders: [
-            {test: /\.jsx?$/, loaders: ['babel-loader']}
-        ]
-    },
-
-    output: {
-        path: path.join(__dirname, 'dist'),
-        filename: 'react-yandex-maps.js',
-        library: 'react-yandex-maps',
-        libraryTarget: 'umd'
-    },
-
-    resolve: {
-        extensions: ['', '.jsx', '.js']
-    },
-
-    plugins: [
-        new webpack.DefinePlugin({
-            'process.env': env
-        }),
-        new webpack.optimize.OccurenceOrderPlugin(),
-        new webpack.optimize.UglifyJsPlugin({
-            compressor: {
-                screw_ie8: true,
-                warnings: false
-            }
-        })
-    ],
-
-    externals: [
-        {
-            'react': {
-                root: 'React',
-                commonjs2: 'react',
-                commonjs: 'react',
-                amd: 'react'
-            }
-        }
+  module: {
+    loaders: [
+      {test: /\.jsx?$/, loaders: ['babel-loader']}
     ]
+  },
+
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'react-yandex-maps.js',
+    library: 'react-yandex-maps',
+    libraryTarget: 'umd'
+  },
+
+  resolve: {
+    extensions: ['', '.jsx', '.js']
+  },
+
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env': env
+    }),
+    new webpack.optimize.OccurenceOrderPlugin(),
+    new webpack.optimize.UglifyJsPlugin({
+      compressor: {
+        screw_ie8: true,
+        warnings: false
+      }
+    })
+  ],
+
+  externals: [
+    {
+      'react': {
+        root: 'React',
+        commonjs2: 'react',
+        commonjs: 'react',
+        amd: 'react'
+      }
+    }
+  ]
 }
