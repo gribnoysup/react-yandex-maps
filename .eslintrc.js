86dfefd73d0438d2c79b9f0383d25c78b65dc0e1
Sergey Petushkov <petushkov.sergey@gmail.com>
2018-03-30 14:12:39 +0200

    Update eslint to use recommended instead of standard; Add prettier config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,19 +1,13 @@
 module.exports = {
-  extends: ['standard', 'standard-react'],
-  rules: {
-    semi: 'off',
-    'jsx-quotes': ['error', 'prefer-double'],
-    'space-before-function-paren': 'off',
-    'comma-dangle': [
-      'error',
-      {
-        arrays: 'never',
-        objects: 'always-multiline',
-        imports: 'always-multiline',
-        exports: 'always-multiline',
-        functions: 'never',
-      },
-    ],
-    'react/no-unused-prop-types': 'warning',
+  parser: 'babel-eslint',
+  extends: ['eslint:recommended', 'plugin:react/recommended'],
+  plugins: ['react'],
+  parserOptions: {
+    ecmaVersion: 2017,
+    sourceType: 'module',
+  },
+  env: {
+    browser: true,
+    es6: true,
   },
 };
