462e893dbee1cf7efd5d1dc6b252b807078eb43a
Sergey Petushkov <petushkov.sergey@gmail.com>
2018-03-30 14:14:08 +0200

    Add JSDoc for util methods

--- src/util/omit.js
+++ src/util/omit.js
@@ -1,3 +1,10 @@
+/**
+ * Helper method to remove keys from the object
+ *
+ * @param {Object} obj Object to remove keys from
+ * @param {string[]} arr List of keys to remove
+ * @returns {Object} Object with omitted keys
+ */
 export const omit = (obj, arr) => {
   const res = {};
   for (var n in obj) {
--- src/util/props.js
+++ src/util/props.js
@@ -1,7 +1,22 @@
+/**
+ * Checks if key exists on an object
+ * 
+ * @param {Object} props Component props
+ * @param {string} key Prop key
+ * @returns {boolean} Check result
+ */
 export function isControlledProp(props, key) {
   return props[key] !== undefined;
 }
 
+/**
+ * Checks if prop exists, otherwise returns "uncontrolled" 
+ * prop that starts with default (e.g., defaultValue)
+ * 
+ * @param {Object} props Component props
+ * @param {string} key Prop key
+ * @return {any} Prop value
+ */
 export function getProp(props, key) {
   return isControlledProp(props, key)
     ? props[key]
