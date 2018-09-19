19f7e4f23c6d0b31a6cb65fc6445fb81a1ddeeff
mapkeji <mapkeji26@gmail.com>
2018-09-19 11:23:42 +0300

    Fix examples babelrc and webpack configuration

--- examples/webpack.config.js
+++ examples/webpack.config.js
@@ -1,47 +1,44 @@
-const path = require('path')
-const webpack = require('webpack')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-const examples = path.resolve(process.cwd(), 'examples')
-const sandbox = path.join(examples, 'src', 'sandbox')
+const examples = path.resolve(process.cwd(), 'examples');
+const sandbox = path.join(examples, 'src', 'sandbox');
 
-const reactYandexMaps = path.resolve(process.cwd(), 'src')
+const reactYandexMaps = path.resolve(process.cwd(), 'src');
 
-const devServer = process.argv[1].indexOf('webpack-dev-server') !== -1
+const devServer = process.argv[1].indexOf('webpack-dev-server') !== -1;
 
-const vendor = [
-  'react',
-  'react-dom',
-  'styled-components'
-]
+const vendor = ['react', 'react-dom', 'styled-components'];
 
-const NODE_ENV = process.env.NODE_ENV || 'development'
+const NODE_ENV = process.env.NODE_ENV || 'development';
 
 module.exports = {
-
   devtool: NODE_ENV === 'development' ? 'source-map' : '',
+
+  mode: NODE_ENV === 'production' ? 'production' : 'development',
 
   devServer: {
     historyApiFallback: true,
-    port: 3000
+    port: 3000,
   },
 
   entry: {
     vendor: vendor,
-    index: examples + '/src/index.js'
+    index: examples + '/src/index.js',
   },
 
   resolve: {
     alias: {
       'react-yandex-maps': reactYandexMaps,
-      'sandbox': sandbox
-    }
+      sandbox: sandbox,
+    },
   },
 
   output: {
     path: examples + '/build',
     publicPath: devServer ? '/' : '/react-yandex-maps/',
-    filename: 'static/js/[name].[chunkhash:8].js'
+    filename: 'static/js/[name].[chunkhash:8].js',
   },
 
   module: {
@@ -50,16 +47,12 @@ module.exports = {
         test: /\.js$/,
         enforce: 'pre',
         exclude: /node_modules/,
-        use: [
-          { loader: 'source-map-loader' }
-        ]
+        use: [{ loader: 'source-map-loader' }],
       },
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        use: [
-          { loader: 'babel-loader' }
-        ]
+        use: [{ loader: 'babel-loader' }],
       },
       {
         test: /\.(gif|png|svg|jpe?g)$/,
@@ -67,32 +60,34 @@ module.exports = {
           {
             loader: 'file-loader',
             options: {
-              name: 'static/assets/[name].[hash:8].[ext]'
-            }
-          }
-        ]
+              name: 'static/assets/[name].[hash:8].[ext]',
+            },
+          },
+        ],
       },
-      {
-        test: /\.json$/,
-        use: [
-          { loader: 'json-loader' }
-        ]
-      }
-    ]
+    ],
+  },
+
+  optimization: {
+    splitChunks: {
+      cacheGroups: {
+        commons: {
+          chunks: 'async',
+          test: /[\\/]node_modules[\\/]/,
+          name: 'vendors',
+          reuseExistingChunk: true,
+        },
+      },
+    },
   },
 
   plugins: [
-    new webpack.optimize.CommonsChunkPlugin({
-        names: ['vendor', 'manifest'],
-        minChunks: Infinity
-    }),
     new webpack.DefinePlugin({
-      'process.env.NODE_ENV': JSON.stringify(NODE_ENV)
+      'process.env.NODE_ENV': JSON.stringify(NODE_ENV),
     }),
     new HtmlWebpackPlugin({
       inject: true,
-      template: examples + '/public/index.html'
-    })
-  ]
-
-}
+      template: examples + '/public/index.html',
+    }),
+  ],
+};
