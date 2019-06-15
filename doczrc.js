8b050c660c8b4b88b8ba3c688090f5989101147a
Sergey Petushkov <petushkov.sergey@gmail.com>
2019-06-15 13:38:13 +0200

    Refactor release script; Add now config: set up routing and deploys; Sort navigation items in docs

--- doczrc.js
+++ doczrc.js
@@ -4,6 +4,26 @@ module.exports = {
   onCreateWebpackChain(config) {
     config
       .plugin('define-plugin')
+      // Add global env variable so we know that we are in
+      // docz when the library is included in the build (allows
+      // us to preconfigure library for the docs)
       .use(DefinePlugin, [{ __DOCZ__: JSON.stringify(true) }]);
   },
+
+  // It doesn't work :'(
+  codeSandbox: false,
+
+  // Sort menu in a more appropriate order
+  menu: [
+    'Getting Started',
+    'Core Principles',
+    'FAQ',
+    'Map',
+    'Panorama',
+    'GeoObjects',
+    'Clusterization',
+    'Controls',
+    'YMaps Context',
+    'Migration Guide',
+  ],
 };
--- scripts/release.sh
+++ scripts/release.sh
@@ -13,72 +13,151 @@ DEFAULT_RELEASE_TYPE="minor"
 RELEASE_TAG="${1:-$DEFAULT_RELEASE_TAG}"
 RELEASE_TYPE="${2:-$DEFAULT_RELEASE_TYPE}"
 
-if [[ $(git diff --shortstat 2> /dev/null | tail -n1) != "" ]]
-then
-  echo "You have uncommited changes, please commit everyting before proceeding with release"
+function checkForChanges() {
+  if [[ $(git diff --shortstat 2>/dev/null | tail -n1) != "" ]]; then
+    echo "You have uncommited changes, please commit everyting before proceeding with release"
+    echo
+
+    exit 1
+  fi
+}
+
+function setEnv() {
+  # For canary releases...
+  if [ "$RELEASE_TAG" == "$CANARY_RELEASE_TAG" ]; then
+    # ... we need to create patch version first
+    PATCH_VERSION=$(npm --no-git-tag-version version patch)
+
+    # ... and then based on the patch and latest commit hash
+    LATEST_COMMIT_HASH=$(git rev-parse --short HEAD)
+    # ... create the release version to be published
+    RELEASE_VERSION=$(npm --no-git-tag-version version "$PATCH_VERSION-$RELEASE_TAG.$LATEST_COMMIT_HASH")
+
+    # Reset release type to canary
+    RELEASE_TYPE=$RELEASE_TAG
+  else
+    RELEASE_VERSION=$(npm --no-git-tag-version version $RELEASE_TYPE)
+  fi
+}
+
+function abortRelease() {
+  echo "⚠️ Publishing aborted"
   echo
+
+  # If the publishing was aborted, restore package.json
+  cp $TMP_PACKAGE_JSON $PACKAGE_JSON
+  # Remove temp package.json file
+  rm -rf $TMP_PACKAGE_JSON
 
   exit 1
-fi
+}
 
-# Copy current package.json just in case
-cp $PACKAGE_JSON $TMP_PACKAGE_JSON
-
-# For canary releases...
-if [ "$RELEASE_TAG" == "$CANARY_RELEASE_TAG" ]; then
-  # ... we need to create patch version first
-  PATCH_VERSION=$(npm --no-git-tag-version version patch)
-
-  # ... and then based on the patch and latest commit hash
-  LATEST_COMMIT_HASH=$(git rev-parse --short HEAD)
-  # ... create the release version to be published
-  RELEASE_VERSION=$(npm --no-git-tag-version version "$PATCH_VERSION-$RELEASE_TAG.$LATEST_COMMIT_HASH")
-
-  # Reset release type to canary
-  RELEASE_TYPE=$RELEASE_TAG
-else 
-  RELEASE_VERSION=$(npm --no-git-tag-version version $RELEASE_TYPE)
-fi
-
-echo "Preparing to publish $RELEASE_TYPE release $RELEASE_VERSION"
-read -p "Is this OK? [y/N] " PROCEED
-echo
-
-if [ "$PROCEED" == "y" ] || [ "$PROCEED" == "Y" ]; then
-  # Reinstall dependencies just in case
-  echo "Installing dependencies"
-  npm install
-
-  # Run tests
-  npm run test
-
-  # Build lib
-  echo "Building distribution assets"
-  npm run build
-
-  # Publish the library
+function confirmRelease() {
+  echo "Preparing to publish $RELEASE_TYPE release $RELEASE_VERSION"
+  read -p "Is this OK? [y/N] " PROCEED
   echo
-  echo "Publishing release to npm"
+
+  if [ "$PROCEED" == "y" ] || [ "$PROCEED" == "Y" ] || [ "$PROCEED" == "yes" ]; then
+    echo "Proceeding with the release"
+  else
+    abortRelease
+  fi
+}
+
+function prepare() {
+  echo "  > Installing dependencies from scratch"
+  echo
+
+  npm ci
+}
+
+function test() {
+  echo "  > Running tests"
+  echo
+
+  npm run test
+}
+
+function build() {
+  echo "  > Building distribution assets"
+  echo
+
+  npm run build
+}
+
+function publish() {
+  echo "  > Publishing release to npm"
   echo
 
   npm publish --access public --tag $RELEASE_TAG
+}
 
-  # Commit, tag and push to github
-  echo "Commiting release to Git"
+function pushVersion() {
+  echo "  > Commiting release to Git"
   echo
 
   if [ "$RELEASE_TAG" == "$CANARY_RELEASE_TAG" ]; then
-    echo "Canary release: skipping commit step, reverting package.json change"
+    echo "️⚠️ Canary release: skipping commit step, reverting package.json change"
     echo
 
     # If canary, skip commit and revert package.json (we will create tag though)
     cp $TMP_PACKAGE_JSON $PACKAGE_JSON
-  else 
-    git commit -am "Release: $RELEASE_TYPE $RELEASE_VERSION"
+  else
+    git commit -am "release($RELEASE_TYPE): $RELEASE_VERSION"
   fi
 
   git tag $RELEASE_VERSION
   git push --tags
+}
+
+function buildDocs() {
+  echo "  > Building the docs assets"
+  echo
+
+  npm run docs:build
+}
+
+function publishDocs() {
+  echo "  > Deploying docs to now"
+  echo
+
+  if [ "$RELEASE_TAG" == "$CANARY_RELEASE_TAG" ]; then
+    echo "⚠️ Canary release: skipping now aliasing"
+    npm run docs:deploy -- --target staging
+    echo
+  else
+    npm run docs:deploy -- --target production
+    echo
+  fi
+}
+
+function main() {
+  # First check if there are any uncommited git changes
+  checkForChanges
+
+  # Copy current package.json just in case
+  cp $PACKAGE_JSON $TMP_PACKAGE_JSON
+
+  # Set environmental variables
+  setEnv
+
+  # Confirm if should proceed with release
+  confirmRelease
+
+  # Reinstall dependencies just in case
+  prepare
+
+  # Run tests
+  test
+
+  # Build lib
+  build
+
+  # Publish the library
+  publish
+
+  # Commit, tag and push to github
+  pushVersion
 
   echo
   echo "============================="
@@ -89,32 +168,17 @@ if [ "$PROCEED" == "y" ] || [ "$PROCEED" == "Y" ]; then
   echo
 
   # Build the docs
-  echo "Building the docs assets"
-  npm run docs:build
+  buildDocs
 
   # Deploy to now
-  echo "Deploying docs to now"
-  npm run docs:deploy
-  echo
+  publishDocs
 
-  if [ "$RELEASE_TAG" == "$CANARY_RELEASE_TAG" ]; then
-    echo "Canary release: skipping now aliasing"
-    echo
-  else
-    npm run docs:alias
-    echo
-  fi
-  
   echo
   echo "Done"
   echo
-else
-  echo "Publishing aborted"
-  echo
 
-  # If the publishing was aborted, restore package.json
-  cp $TMP_PACKAGE_JSON $PACKAGE_JSON
-fi
+  # Remove temp package.json file
+  rm -rf $TMP_PACKAGE_JSON
+}
 
-# Remove temp package.json file
-rm -rf $TMP_PACKAGE_JSON
+main
